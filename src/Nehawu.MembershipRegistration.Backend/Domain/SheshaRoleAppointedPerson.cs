433f34eba1b9efeacd80de00e79cc0c2aa9352d9
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-07-26 10:37:32 +0200

    Added ShashaRoleAppointedPerson domain properties

--- src/Nehawu.MembershipRegistration.Backend/Domain/SheshaRoleAppointedPerson.cs
+++ src/Nehawu.MembershipRegistration.Backend/Domain/SheshaRoleAppointedPerson.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Nehawu.MembershipRegistration.Backend.Domain.RefList;
+using Shesha.Domain;
+using Shesha.Domain.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Nehawu.MembershipRegistration.Backend.Domain
 {
-    class SheshaRoleAppointedPerson
+    /// <summary>
+    /// SheshaRoleAppointedPerson domain
+    /// </summary>
+    [Entity(TypeShortAlias = "Eclb.SheshaRoleAppointedPerson")]
+    public class SheshaRoleAppointedPerson : ShaRoleAppointedPerson
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual DateTime ValidFrom { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual DateTime? ValidTo { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual RefListMembershipStatus Status { get; set; }
     }
 }
