20408ea62349a5f8999f026628e63b3ff748315c
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-07-26 10:27:25 +0200

    Added payment status refList properties

--- src/Nehawu.MembershipRegistration.Backend/Domain/RefList/RefListPaymentStatus.cs
+++ src/Nehawu.MembershipRegistration.Backend/Domain/RefList/RefListPaymentStatus.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Shesha.Domain.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Nehawu.MembershipRegistration.Backend.Domain.RefList
 {
-    class RefListPaymentStatus
+    [ReferenceList("Eclb", "RefListPaymentStatus")]
+    public enum RefListPaymentStatus : long
     {
+        Outstanding = 1,
+        Paid = 2
     }
 }
