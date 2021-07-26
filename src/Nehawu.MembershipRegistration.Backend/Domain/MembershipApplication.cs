787420ff09a0a2c3cb2ef80abd416933ac2b65e6
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-07-26 10:33:46 +0200

    Added MembershipApplication domain properties

--- src/Nehawu.MembershipRegistration.Backend/Domain/MembershipApplication.cs
+++ src/Nehawu.MembershipRegistration.Backend/Domain/MembershipApplication.cs
@@ -1,10 +1,79 @@
-﻿using System;
+﻿using Abp.Domain.Entities.Auditing;
+using Nehawu.MembershipRegistration.Backend.Domain.RefList;
+using Shesha.Domain;
+using Shesha.Domain.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Nehawu.MembershipRegistration.Backend.Domain
 {
-    class MembershipApplication
+    [Entity(TypeShortAlias = "Eclb.MembershipApplication")]
+    public class MembershipApplication : FullAuditedEntity<Guid>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string Surname { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string IDNumber { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string Cellphone { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string Email { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string ResidentialAddress { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual Area Province { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual Area Region { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual Area Branch { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual DateTime ApplicationDate { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual RefListApplicationStatus ApplicationStatus { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public virtual Member RegisteredMember { get; set; }
     }
 }
