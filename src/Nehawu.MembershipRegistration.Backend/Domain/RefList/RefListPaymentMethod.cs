5a509ff88bd7a9fc6e84c7aacb93efb8fe9f70f3
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-07-26 10:26:40 +0200

    Added payment method refList properties

--- src/Nehawu.MembershipRegistration.Backend/Domain/RefList/RefListPaymentMethod.cs
+++ src/Nehawu.MembershipRegistration.Backend/Domain/RefList/RefListPaymentMethod.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Shesha.Domain.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Nehawu.MembershipRegistration.Backend.Domain.RefList
 {
-    class RefListPaymentMethod
+    [ReferenceList("Eclb", "RefListPaymentMethod")]
+    public enum RefListPaymentMethod : long
     {
+        Cash = 1,
+        EFT = 2,
+        DebitOrder = 3
     }
 }
