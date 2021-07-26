2220279bf9f79ccc83eee647831c15a6cb20ffd8
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-07-26 10:25:04 +0200

    Added application status reference list properties

--- src/Nehawu.MembershipRegistration.Backend/Domain/RefList/RefListApplicationStatus.cs
+++ src/Nehawu.MembershipRegistration.Backend/Domain/RefList/RefListApplicationStatus.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Shesha.Domain.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Nehawu.MembershipRegistration.Backend.Domain.RefList
 {
-    class RefListApplicationStatus
+    [ReferenceList("Eclb", "RefListApplicationStatus")]
+    public enum RefListApplicationStatus : long
     {
+        ApplicationSubmitted = 1,
+        Applicationprocessing = 2,
+        ApplicationSucessful = 3,
+        ApplicationRejected = 4
     }
 }
