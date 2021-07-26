99dac7e48265defa72f3b2f61777bfe110776cc2
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-07-26 10:35:24 +0200

    Added Payment domain properties

--- src/Nehawu.MembershipRegistration.Backend/Domain/Payment.cs
+++ src/Nehawu.MembershipRegistration.Backend/Domain/Payment.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using Abp.Domain.Entities.Auditing;
+using Nehawu.MembershipRegistration.Backend.Domain.RefList;
+using Shesha.Domain.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Nehawu.MembershipRegistration.Backend.Domain
 {
-    class Payment
+    /// <summary>
+    /// Payment Domain
+    /// </summary>
+    [Entity(TypeShortAlias = "Eclb.Payment")]
+    public class Payment : FullAuditedEntity<Guid>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual Member Member { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string PaymentRef { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual DateTime PaymentDate { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual decimal PaymentAmount { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual RefListPaymentStatus PaymentStatus { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual RefListPaymentMethod PaymentMethod { get; set; }
     }
 }
