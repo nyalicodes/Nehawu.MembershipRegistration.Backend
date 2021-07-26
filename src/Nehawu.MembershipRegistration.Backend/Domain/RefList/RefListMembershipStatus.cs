19784000c2762b85bfe09e3cafe77cd352f25e77
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-07-26 10:25:59 +0200

    Added membership status refList properties

--- src/Nehawu.MembershipRegistration.Backend/Domain/RefList/RefListMembershipStatus.cs
+++ src/Nehawu.MembershipRegistration.Backend/Domain/RefList/RefListMembershipStatus.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Shesha.Domain.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Nehawu.MembershipRegistration.Backend.Domain.RefList
 {
-    class RefListMembershipStatus
+    [ReferenceList("Eclb", "RefListMembershipStatus")]
+    public enum RefListMembershipStatus : long
     {
+        Active = 1,
+        Suspended = 2,
+        Cancelled = 3
     }
 }
