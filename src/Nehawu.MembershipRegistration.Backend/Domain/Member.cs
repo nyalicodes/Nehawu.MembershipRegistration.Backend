c6eeef6240be0357a05b6ee661d9b0cb11a32415
Nyaladzi Trust Tjibha <43335962+nyalicodes@users.noreply.github.com>
2021-07-26 10:31:26 +0200

    Added Member domain properties

--- src/Nehawu.MembershipRegistration.Backend/Domain/Member.cs
+++ src/Nehawu.MembershipRegistration.Backend/Domain/Member.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using Nehawu.MembershipRegistration.Backend.Domain.RefList;
+using Shesha.Domain;
+using Shesha.Domain.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Nehawu.MembershipRegistration.Backend.Domain
 {
-    class Member
+    /// <summary>
+    /// Member domain class inheriting properties from the Shesha Person domain
+    /// </summary>
+    [Entity(TypeShortAlias = "Eclb.Member")]
+    [Discriminator]
+    public class Member : Person
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string MembershipNumber { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual string ResidentialAddress { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual decimal? ResidentialAddressLat { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual decimal? ResidentialAddressLong { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual RefListMembershipStatus MembershipStatus { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual DateTime? MembershipStartDate { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual DateTime? MembershipEndDate { get; set; }
     }
 }
